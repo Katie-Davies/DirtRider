e03c434f216cb146279fcf8b76d2f568aa8fb0eb
Katie Davies <millerkatie1990@gmail.com>
2024-04-01 16:10:51 +1300

    add browse all button

--- client/components/Homepage.tsx
+++ client/components/Homepage.tsx
@@ -7,22 +7,20 @@ function Homepage() {
         <div className="home-main-bg">
           <Searchbar />
         </div>
+
         <div className="home-main-card">
-          <img
-            src="public/images/mainpageSmallicon1.png"
-            alt="muddy bike wheel"
-          />
+          <img src="/images/mainpageSmallicon1.png" alt="muddy bike wheel" />
           <h1>The sign of a good day!</h1>
         </div>
         <div className="home-main-card">
           <h1>Riding with friends!</h1>
           <img
-            src="public/images/mainpageSmallicon2.png"
+            src="/images/mainpageSmallicon2.png"
             alt="two dirtbikes in field"
           />
         </div>
         <div className="home-main-card">
-          <img src="public/images/mainpageSmallicon3.png" alt="muddy boot" />
+          <img src="/images/mainpageSmallicon3.png" alt="muddy boot" />
           <h1>No better feeling!</h1>
         </div>
       </div>
