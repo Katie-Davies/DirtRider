593e49e11bdf3e6bf14d0043b2612e703b7bdd06
Katie Davies <millerkatie1990@gmail.com>
2024-04-01 15:58:22 +1300

    add placeholder form

--- client/components/Searchbar.tsx
+++ client/components/Searchbar.tsx
@@ -1,8 +1,14 @@
 function Searchbar() {
   return (
-    <>
-      <form></form>
-    </>
+    <div className="searchbar">
+      <form>
+        <input type="text" placeholder="Make" name="make" />
+        <input type="text" placeholder="Model" name="model" />
+        <input type="text" placeholder="Engine Size" name="engine" />
+        <input type="text" placeholder="Location" name="location" />
+        <button type="submit">Search</button>
+      </form>
+    </div>
   )
 }
 
