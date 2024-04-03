71d51e62779fbe56627f7b779db171f0b1a44b81
Katie Davies <millerkatie1990@gmail.com>
2024-04-03 13:52:35 +1300

    add models

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,3 +1,4 @@
+import { Bikes, User } from '../../models/models'
 import connection from './connection'
 
 const db = connection
@@ -13,12 +14,20 @@ export async function getBikeById(id: string) {
 }
 
 //POST add bikes
-export async function addBike(data) {
+export async function addBike(data: Bikes) {
   return await db('bikes').insert(data)
 }
 
 //PATCH update bike
-
+export async function updateBike(id: number, data: Bikes) {
+  return db('bikes').where({ id }).update(data)
+}
 //GET user by id
-
+export async function getUserById(id: User) {
+  const user = db('users').select().where({ id }).first()
+  return user
+}
 //PATCH update user
+export async function updateUser(id: string, data: User) {
+  return db('user').where({ id }).update(data)
+}
