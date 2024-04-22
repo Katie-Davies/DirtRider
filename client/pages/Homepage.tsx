c05cece2af025b701a0386879bf0002ab4ff3e80
Katie Davies <millerkatie1990@gmail.com>
2024-04-22 15:12:23 +1200

    restyle homepage

--- client/pages/Homepage.tsx
+++ client/pages/Homepage.tsx
@@ -7,12 +7,20 @@ function Homepage() {
     <>
       <div className="home-main">
         <div className="home-main-bg">
+          <div>
+            <h1 className="text-4xl text-center text-white">
+              Stop wishing and start hiring!
+            </h1>
+            <h1 className="text-2xl text-center text-white">
+              Find a bike today!
+            </h1>
+          </div>
           <Link to="/bikes">
-            <Button className="opacity-80">View Bikes</Button>
+            <Button className="opacity-80 mt-10">View Bikes</Button>
           </Link>
         </div>
 
-        <div className="home-main-card">
+        {/* <div className="home-main-card">
           <img src="/images/mainpageSmallicon1.png" alt="muddy bike wheel" />
           <h1>The sign of a good day!</h1>
         </div>
@@ -26,7 +34,7 @@ function Homepage() {
         <div className="home-main-card">
           <img src="/images/mainpageSmallicon3.png" alt="muddy boot" />
           <h1>No better feeling!</h1>
-        </div>
+        </div> */}
       </div>
     </>
   )
