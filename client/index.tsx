1f00848844083c9a79bf769a3dca75def9d0cc62
Katie Davies <millerkatie1990@gmail.com>
2024-04-23 16:46:43 +1200

    setup auth0

--- client/index.tsx
+++ client/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
+import { Auth0Provider } from '@auth0/auth0-react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import routes from './routes.tsx'
 
@@ -10,9 +10,20 @@ const queryClient = new QueryClient()
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>,
+    <Auth0Provider
+      domain="mealeasy-2024.au.auth0.com"
+      clientId="doliuGIKF1MKZOopd0UNHk1GbgY5ZVSc"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: 'https://dirtrider/api',
+      }}
+      useRefreshTokens
+      cacheLocation="localstorage"
+    >
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </Auth0Provider>,
   )
 })
