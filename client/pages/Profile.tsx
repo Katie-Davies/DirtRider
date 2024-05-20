71b699fe89b7ab9658c9d33e596cf51dc8309256
Katie Davies <millerkatie1990@gmail.com>
2024-05-21 08:56:18 +1200

    create basic profile page

--- client/pages/Profile.tsx
+++ client/pages/Profile.tsx
@@ -1,5 +1,27 @@
+const fakeData = {
+  userName: 'JohnnyDoe123',
+  name: 'John Doe',
+  email: 'johnDoe123@mail.com',
+  age: 25,
+  location: 'Taupo',
+}
+
 function Profile() {
-  return <h1>Profiles</h1>
+  return (
+    <div>
+      <h1>Your Profile</h1>
+      <div>
+        <p>Username: {fakeData.userName}</p>
+        <p>Name: {fakeData.name}</p>
+        <p>Email: {fakeData.email}</p>
+        <p>Age: {fakeData.age}</p>
+        <p>Location: {fakeData.location}</p>
+      </div>
+      <div>
+        <button>Edit Profile</button>
+      </div>
+    </div>
+  )
 }
 
 export default Profile
