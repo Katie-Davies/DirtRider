e58adbc16d0b0abf33478b69c01fbd5251a40533
Katie Davies <millerkatie1990@gmail.com>
2024-06-09 18:14:02 +1200

    range selector working

--- client/pages/BookingForm.tsx
+++ client/pages/BookingForm.tsx
@@ -1,10 +1,53 @@
 import { useParams } from 'react-router-dom'
+import useGetBikeByID from '../hooks/useGetBikeById'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import { useState } from 'react'
+import { format } from 'date-fns'
 
 function BookingForm() {
   const { id } = useParams()
-  const { data: bike, isLoading, isError } = useGetBikeByID(id)
-  return
-  ;<h1>Booking Form</h1>
+  const { data: bike, isLoading, isError } = useGetBikeByID(id ?? '')
+  const [startDate, setStartDate] = useState(null)
+  const [endDate, setEndDate] = useState(null)
+
+  const formatDate = (date) => (date ? format(date, 'dd-MM-yyyy') : '')
+
+  if (isLoading) {
+    console.log('Loading bike')
+    return <span className="loading loading-spinner loading-md"></span>
+  }
+  if (isError) {
+    console.log('Error finding bike')
+    return <p>There is an error loading the bike</p>
+  }
+
+  if (bike) {
+    return (
+      <>
+        <h1>Booking Form</h1>
+        <form>
+          <label></label>
+          <DatePicker
+            placeholderText="start"
+            onChange={(date) => {
+              const [start, end] = date
+              setStartDate(start)
+              setEndDate(end)
+            }} // Fix: Provide a default value for the date parameter
+            startDate={startDate}
+            endDate={endDate}
+            dateFormat="dd-MM-yyyy"
+            selectsRange={true}
+            isClearable={true}
+          />
+        </form>
+
+        <p>{formatDate(startDate)}</p>
+        <p>{formatDate(endDate)}</p>
+      </>
+    )
+  }
 }
 
 export default BookingForm
