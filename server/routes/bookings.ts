7e8bcf6a759bc2805f5da16531ff8c1defd4ec10
Katie Davies <millerkatie1990@gmail.com>
2024-06-12 12:44:59 +1200

    routes for host and users bikes

--- server/routes/bookings.ts
+++ server/routes/bookings.ts
@@ -3,3 +3,27 @@ import { Router } from 'express'
 import * as db from '../db/db'
 
 const router = Router()
+
+// Get renter Bookings
+router.get('/renter/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    const bookings = await db.getRentersBookings(id)
+    res.status(200).json(bookings)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Error retureving bookings' })
+  }
+})
+
+// Get host Bookings
+router.get('/host/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    const bookings = await db.getHostsBookings(id)
+    res.status(200).json(bookings)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Error retureving bookings' })
+  }
+})
