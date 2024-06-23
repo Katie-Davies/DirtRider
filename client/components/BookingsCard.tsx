7ddb1e5a70f8fb1924297f6b74f9643711acbc6c
Katie Davies <millerkatie1990@gmail.com>
2024-06-23 21:07:42 +1200

    set up for both current and previous

--- client/components/BookingsCard.tsx
+++ client/components/BookingsCard.tsx
@@ -1,3 +1,5 @@
+import Button from './Button'
+
 interface Props {
   key: number
   id: number
@@ -5,22 +7,27 @@ interface Props {
   model: string
   start_date: string
   end_date: string
+  current: boolean
 }
 
 export function BookingsCard(booking: Props) {
   return (
     <div key={booking.id}>
-      <div className="card w-96 bg-white mt-7 h-auto border-2">
+      <div className="card bg-white mt-7 h-auto border-2 w-96">
         <div className="card-body">
           <h3 className="card-title">
             {booking.make} {booking.model}
           </h3>
-          <p>{booking.start_date}</p>
-          <p>{booking.end_date}</p>
-          <div className="card-actions justify-end">
-            <button className="btn mb-1">Update</button>
-            <button className="btn">Cancel</button>
+          <div className="flex ">
+            <p>From: {booking.start_date}</p>
+            <p>To: {booking.end_date}</p>
           </div>
+          {booking.current ? (
+            <div className="card-actions justify-end">
+              <Button className="btn bg-customBlue text-white">Update</Button>
+              <Button className="btn  bg-customBlue text-white">Cancel</Button>
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
