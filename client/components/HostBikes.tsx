190954fb2f25940f824bb900022c34d7a6416290
Katie Davies <millerkatie1990@gmail.com>
2024-06-30 18:39:01 +1200

    input field showing

--- client/components/HostBikes.tsx
+++ client/components/HostBikes.tsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
 import { useGetHostsBikes } from '../hooks/useGetHostsBikes'
-import { BikeId } from '../../models/models'
+
 import SmallBikeCard from './SmallBikeCard'
 
 interface Props {
@@ -9,9 +8,7 @@ interface Props {
 
 function HostBikes({ user }: Props): JSX.Element {
   const { data: bikes, isLoading, isError } = useGetHostsBikes(user ?? '')
-  const [bike, setBike] = useState<BikeId | null>(null)
 
-  const handleUpdatePrice = (bike: BikeId) => {}
   if (isLoading) {
     return <h1>Your bikes are loading...</h1>
   }
@@ -25,23 +22,6 @@ function HostBikes({ user }: Props): JSX.Element {
         <h1>My Bikes </h1>
         {bikes.map(
           (bike) => (
-            // return (
-            //   <div key={bike.id}>
-            //     <h2>
-            //       {bike.make} {bike.model}
-            //     </h2>
-            //     {bike.image ? (
-            //       <img src={`public/images/${bike.image}`} alt={bike.model} />
-            //     ) : null}
-
-            //     <p>{bike.price}</p>
-            //     <button className="border-4 border-customBlue rounded-md p-2 bg-customBlue text-white">
-            //       Update Price
-            //     </button>
-            //     <button className="border-4 border-customBlue rounded-md p-2 bg-customBlue text-white">
-            //       Delete Bike
-            //     </button>
-            //   </div>
             <SmallBikeCard key={bike.id} {...bike} />
           ),
           // )
--- client/components/SmallBikeCard.tsx
+++ client/components/SmallBikeCard.tsx
@@ -1,6 +1,28 @@
+import { useState } from 'react'
 import { BikeId } from '../../models/models'
+import { updateBike } from '../apis/apiClient'
 
 function SmallBikeCard(bike: BikeId): JSX.Element {
+  const [updatedBike, setUpdatedBike] = useState<BikeId>({
+    id: bike.id,
+    make: bike.make,
+    model: bike.model,
+    engine_size: bike.engine_size,
+    location: bike.location,
+    user_authid: bike.user_authid,
+    price: bike.price,
+    image: bike.image,
+  })
+  const [updatePrice, setUpdatePrice] = useState<boolean>(false)
+  const handleUpdate = () => {
+    setUpdatePrice(true)
+    console.log('update price')
+  }
+
+  const handleUpdatePrice = () => {
+    updateBike(updatedBike)
+    console.log('bike has been updated')
+  }
   return (
     <div key={bike.id}>
       <h2>
@@ -11,9 +33,29 @@ function SmallBikeCard(bike: BikeId): JSX.Element {
       ) : null}
 
       <p>{bike.price}</p>
-      <button className="border-4 border-customBlue rounded-md p-2 bg-customBlue text-white">
+      <button
+        className="border-4 border-customBlue rounded-md p-2 bg-customBlue text-white"
+        onClick={handleUpdate}
+      >
         Update Price
       </button>
+      {updatePrice ? (
+        <>
+          <input
+            type="number"
+            value={updatedBike.price}
+            onChange={(e) =>
+              setUpdatedBike({
+                ...updatedBike,
+                price: parseInt(e.target.value),
+              })
+            }
+            onSubmit={handleUpdatePrice}
+          />
+          <button onClick={handleUpdatePrice}>Submit</button>
+        </>
+      ) : null}
+
       <button className="border-4 border-customBlue rounded-md p-2 bg-customBlue text-white">
         Delete Bike
       </button>
