cbfb0364de3ebefbcc5518e0d48e079b1b7f42a4
Katie Davies <millerkatie1990@gmail.com>
2024-09-03 22:39:55 +1200

    create edit booking component & route

--- client/components/BookingsCard.tsx
+++ client/components/BookingsCard.tsx
@@ -1,5 +1,5 @@
 import useDeleteBooking from '../hooks/useDeleteBooking'
-import { deleteBooking } from '../apis/apiClient'
+
 import Button from './Button'
 
 interface Props {
---
+++ client/components/EditBooking.tsx
@@ -0,0 +1,9 @@
+function EditBooking() {
+  return (
+    <div>
+      <h1>Edit Booking</h1>
+    </div>
+  )
+}
+
+export default EditBooking
